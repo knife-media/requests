3c35e15e8ada62c61f60f44101e4ffd9b1dccdc8
Anton Lukin <anton@lukin.me>
2021-05-07 00:10:59 +0300

    replace file_get_contents sender with curl

--- senders/SenderTelegram.php
+++ senders/SenderTelegram.php
@@ -15,15 +15,22 @@ class SenderTelegram
      */
     public static function send_message($message, $mode = 'HTML')
     {
+        $curl = curl_init();
+
         if (empty($message['parse_mode'])) {
             $message['parse_mode'] = $mode;
         }
 
         // Make Telegram send message url
-        $url = self::$api . $_ENV['TELEGRAM_TOKEN'] . "/sendMessage?";
+        $url = self::$api . $_ENV['TELEGRAM_TOKEN'] . "/sendMessage";
 
-        // Try to send message
-        $result = @file_get_contents($url . http_build_query($message));
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $message);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+        curl_close($curl);
 
         return $result;
     }
